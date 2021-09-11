f3d44f057df4bc1c8ec381600aac3cdfa4981bc1
Skimige <me@xyx.moe>
2021-09-11 23:43:54 +0800

    fix: remove unwanted default patches

--- user/cr6606/custom.sh
+++ user/cr6606/custom.sh
@@ -2,6 +2,6 @@
 
 echo "Applying patch with CR660x support"
 wget https://github.com/openwrt/openwrt/pull/4118.patch
-git apply 4118.patch
+git apply 4118.patch || true
 rm 4118.patch
 echo "Patch done"
